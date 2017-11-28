69cd9f756d41cf4fd629274e6dca582b43a57eef
Benjamin Creem <benjamincreem@gmail.com>
2017-11-28 12:35:41 -0500

    Added calculating file size, number of records, number of records per thread

--- sortMerge.c
+++ sortMerge.c
@@ -33,19 +33,26 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Can't open file\n");
 		exit(1);
 	}
-	int FileSize;
-	printf("Num Threads: %d\nFile Name: %s\n", nThreads, argv[2]);
+	fseek(file, 0L, SEEK_END);
+	int FileSize = ftell(file);
+	rewind(file);
+	printf("Num Threads: %d\nFile Name: %s\nFile Size: %d\n", 
+	nThreads, argv[2], FileSize);
 	
 	
 	//Number of records
-	//int nRecs = FileSize / RECSIZE;
-
+	int nRecs = FileSize / RECSIZE;
+	printf("The number of records is: %d\n", nRecs);
+	
 	//Number of Records per thread
-	//int nRecsPerThd = nRecs / nThreads;
+	int nRecsPerThd = nRecs / nThreads;
+	printf("The number of records per thread is: %d\n", nRecsPerThd);
 
 	//n is the number of processors
 	int n = sysconf(_SC_NPROCESSORS_ONLN);
 	printf("The number of cores is: %d\n", n);
+
+
 
 	//Pointer to Array of Thread IDs
 	static pthread_t *tids;
